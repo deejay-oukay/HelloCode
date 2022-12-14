364e6b2fe1a993d17774546ed98631bb697759be
deejay-oukay <dj-ok@mail.ru>
2022-12-14 16:57:52 +0500

    If-Else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Как Вас зовут? ");
+string username = Console.ReadLine();
+if (username.ToLower() == "константин")
+    Console.WriteLine("Здравствуй, хозяин");
+else
+    Console.WriteLine("Привет, "+username+"!");
